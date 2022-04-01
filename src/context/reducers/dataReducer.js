50550dd21b71293ba8408bc6f42bd46eb1359540
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-04-02 01:14:30 +0530

    added liked api calls and reducer logic

--- src/context/reducers/dataReducer.js
+++ src/context/reducers/dataReducer.js
@@ -43,6 +43,49 @@ const userReducer = (state, action) => {
         ...state,
         vid: { ...state.vid, error: payload, loading: false, items: [] },
       };
+
+    //Likes
+
+    case likeConstants.LOADING:
+      return {
+        ...state,
+        liked: { ...state.liked, loading: true },
+      };
+
+    case likeConstants.GET_LIKED:
+      return {
+        ...state,
+        liked: {
+          error: null,
+          loading: false,
+          items: payload,
+        },
+      };
+
+    case likeConstants.ERROR:
+      return {
+        ...state,
+        liked: { error: payload, loading: false, items: [] },
+      };
+
+    case likeConstants.ADD_TO_LIKED:
+      return {
+        ...state,
+        liked: {
+          error: null,
+          loading: false,
+          items: payload,
+        },
+      };
+
+    case likeConstants.REMOVE_FROM_LIKED:
+      return {
+        ...state,
+        liked: {
+          ...state.liked,
+          items: payload,
+        },
+      };
   }
 };
 
--- src/services/likes.js
+++ src/services/likes.js
@@ -1,1 +1,61 @@
 import axios from "axios";
+
+import { likeConstants } from "../context";
+
+//Videos
+export const loadLikedVideos = async (dispatch) => {
+  try {
+    dispatch({ type: likeConstants.LOADING });
+    const response = await axios.get(`${process.env.REACT_APP_URL}/user/likes`);
+
+    if (response.status === 200) {
+      dispatch({
+        type: likeConstants.GET_LIKED,
+        payload: response.data.likes,
+      });
+    }
+  } catch (e) {
+    dispatch({
+      type: likeConstants.ERROR,
+      payload: "Oops! Something went wrong :(",
+    });
+  }
+};
+
+export const addToLiked = async (video, dispatch, openAlert) => {
+  try {
+    const response = await axios.post(
+      `${process.env.REACT_APP_URL}/api/user/likes`,
+      config,
+      video
+    );
+
+    if (response.status === 200) {
+      openAlert({ message: "Added to Liked videos!", type: "info" });
+      dispatch({
+        type: likeConstants.ADD_TO_LIKED,
+        payload: response.data.likes,
+      });
+    }
+  } catch (e) {
+    openAlert({ message: e?.response?.data?.errors[0], type: "error" });
+  }
+};
+
+export const removeFromLiked = async (id, dispatch, openAlert) => {
+  try {
+    const response = await axios.delete(
+      `${process.env.REACT_APP_URL}/user/likes/${id}`
+    );
+
+    if (response.status === 200) {
+      openAlert({ message: "Removed from Liked videos!", type: "info" });
+      dispatch({
+        type: likeConstants.REMOVE_FROM_LIKED,
+        payload: response.data.likes,
+      });
+    }
+  } catch (e) {
+    openAlert({ message: e?.response?.data?.errors[0], type: "error" });
+  }
+};
